9b0dd10457d064c82e9a8ed13954f2764c44700b
wang <960401828@qq.com>
2022-09-13 09:10:33 +0800

    first commit

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+//动态内存 总结：
+//结构体中最后一个元素大小是未知的 这就叫做柔型数组的成员
+
+struct S{
+    int n;
+    int arr[0]; //柔性数组成员前面至少有一个其他成员
+};
+
 int main() {
-    printf("Hello, World!\n");
+
+
+
     return 0;
 }
