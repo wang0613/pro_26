3adf9bd29256d5ecd3af8beb1d189f8cb78b3cce
wang <960401828@qq.com>
2022-09-13 09:29:14 +0800

    second commit

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //动态内存 总结：
 //结构体中最后一个元素大小是未知的 这就叫做柔型数组的成员
@@ -10,7 +11,33 @@ struct S{
 
 int main() {
 
+//    printf("%d\n",sizeof(struct S)); //4
+    //为arr开辟20个字节
+    struct S* ps = (struct S*)malloc(sizeof(struct S)+5*sizeof(int));
+    ps->n = 100;
+    int i = 0;
+    for(i = 0; i<5;i++)
+    {
+        ps->arr[i] = i;
+    }
 
 
+    //重新开辟
+    struct S* pps = (struct S*)realloc(ps,40);
+    if(pps != NULL)
+    {
+        ps = pps;
+    }
+    for(i = 1;i <10; i++)
+    {
+        ps->arr[i] = i;
+    }
+
+    for( i = 0; i <10; i++)
+    {
+        printf("%d ",ps->arr[i]);
+    }
+    free(pps);
+    pps= NULL;
     return 0;
 }
